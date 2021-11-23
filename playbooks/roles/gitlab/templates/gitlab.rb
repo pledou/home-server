517a61752192e70b73dbcda48d79fe8849227e0d
Pierre Leduc <p.leduc@mailo.com>
2021-11-23 11:37:25 +0100

    gitlab init

---
+++ playbooks/roles/gitlab/templates/gitlab.rb
@@ -0,0 +1,44 @@
+external_url 'https://gitlab.{{app_domain_name}}/'
+gitlab_rails['initial_root_password'] = File.read('/run/secrets/gitlab_root_password')
+
+letsencrypt['enable'] = false
+
+nginx['listen_port'] = 80
+nginx['listen_https'] = false
+nginx['http2_enabled'] = false
+
+nginx['proxy_set_headers'] = {
+  "Host" => "$http_host",
+  "X-Real-IP" => "$remote_addr",
+  "X-Forwarded-For" => "$proxy_add_x_forwarded_for",
+  "X-Forwarded-Proto" => "https",
+  "X-Forwarded-Ssl" => "on"
+}
+
+gitlab_rails['gitlab_shell_ssh_port'] = 22
+
+registry_external_url 'https://registry.gitlab.{{app_domain_name}}'
+registry_nginx['listen_port'] = 5100
+registry_nginx['listen_https'] = false
+registry_nginx['proxy_set_headers'] = {
+  "Host" => "$http_host",
+  "X-Real-IP" => "$remote_addr",
+  "X-Forwarded-For" => "$proxy_add_x_forwarded_for",
+  "X-Forwarded-Proto" => "https",
+  "X-Forwarded-Ssl" => "on"
+}
+
+pages_external_url 'https://pages.gitlab.{{app_domain_name}}'
+pages_nginx['listen_port'] = 5200
+pages_nginx['listen_https'] = false
+
+pages_nginx['proxy_set_headers'] = {
+  "Host" => "$http_host",
+  "X-Real-IP" => "$remote_addr",
+  "X-Forwarded-For" => "$proxy_add_x_forwarded_for",
+  "X-Forwarded-Proto" => "https",
+  "X-Forwarded-Ssl" => "on"
+}
+
+gitlab_pages['inplace_chroot'] = true
+gitlab_pages['external_http'] = ['gitlab:5201']
